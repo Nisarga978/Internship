ac4e1d8f53fd2694efd2076019e28a2402bc5514
Nisarga <nisargalokesh2@gmail.com>
2026-02-10 11:30:20 +0530

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,28 +1,19 @@
-<<<<<<< HEAD
+
 class Payment:
     def pay(self):
         print("Processing generic payment...")
-
-
 
 class GooglePay(Payment):
     def pay(self):
         print("Payment done using Google Pay.")
 
-
-
 class PhonePe(Payment):
     def pay(self):
         print("Payment done using PhonePe.")
 
-
-
 class CreditCard(Payment):
     def pay(self):
         print("Payment done using Credit Card.")
-
-
-
 p1 = Payment()
 p2 = GooglePay()
 p3 = PhonePe()
@@ -32,39 +23,4 @@ p4 = CreditCard()
 p1.pay()
 p2.pay()
 p3.pay()
-=======
-class Payment:
-    def pay(self):
-        print("Processing generic payment...")
 
-
-
-class GooglePay(Payment):
-    def pay(self):
-        print("Payment done using Google Pay.")
-
-
-
-class PhonePe(Payment):
-    def pay(self):
-        print("Payment done using PhonePe.")
-
-
-
-class CreditCard(Payment):
-    def pay(self):
-        print("Payment done using Credit Card.")
-
-
-
-p1 = Payment()
-p2 = GooglePay()
-p3 = PhonePe()
-p4 = CreditCard()
-
-
-p1.pay()
-p2.pay()
-p3.pay()
->>>>>>> 09b63ad (Initial Commit)
-p4.pay()
